6bfbf30657927c663ee9a21e501c1bec6894eb54
martin-k-kamir <64164128+Martin-K-Kamir@users.noreply.github.com>
2022-11-07 23:46:37 +0100

    w

--- src/react/components/PageLabel.jsx
+++ src/react/components/PageLabel.jsx
@@ -1,7 +1,11 @@
 export default function PageLabel(props) {
 	return (
 		<div className="page-label" aria-hidden={true}>
-			<div>{props.page}</div>
+			<div className="page-label__container">
+				<div className="[ page-label__content ] [ fg-neutral-3 fw-2 ]">
+					{props.page}
+				</div>
+			</div>
 		</div>
 	);
 }
