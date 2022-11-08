de821ca4ed90695dc102161d85b93760177332de
Martin Kamír <martin.k.kamir@gmail.com>
2022-11-08 16:59:21 +0100

    w

--- src/react/components/Image.jsx
+++ src/react/components/Image.jsx
@@ -1,13 +1,14 @@
 export default function Image(props) {
 
-	console.log(props)
 
 	return (
 		<picture className={props.classes}>
-			{props.widths && <source sizes={props.sizes ? props.sizes : ''} srcSet={`${props.widths.map(width => (
-						`assets/images/${props.dir}/${props.name}-${width}.webp ${width}w,
-						assets/images/${props.dir}/${props.name}-${width}.jpg ${width}w`
-					))}`}/>
+			{props.widths && <>
+				<source sizes={props.sizes ? props.sizes : ''} type="image/webp" srcSet={`${props.widths.map(width => (
+						`assets/images/${props.dir}/${props.name}-${width}.webp ${width}w`))}`}/>
+				<source sizes={props.sizes ? props.sizes : ''} type="image/jpeg" srcSet={`${props.widths.map(width => (
+						`assets/images/${props.dir}/${props.name}-${width}.jpg ${width}w`))}`}/>
+			</>
 			}
 
 			<img src={`assets/images/${props.dir}/${props.widths ? props.name + '-' + Math.max(...props.widths) : props.name}.jpg`}
